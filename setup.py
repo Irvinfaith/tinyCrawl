86facebaf76bac999645c18638bb44ce10cfcc4f
Irvinfaith <waxiguan00@hotmail.com>
2021-01-11 11:03:47 +0800

    [v0.1.1]-release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from setuptools import setup, find_packages
 with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
-__version__ = '0.1'
+__version__ = '0.1.1'
 
 setup(
     name="tinyCrawl",
